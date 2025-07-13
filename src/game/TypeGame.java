e88afe16bb2abd1da01d0eeb15e7ee335c0f69ab
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2025-07-13 15:32:53 +0530

    set the color at words ui #2

--- src/game/TypeGame.java
+++ src/game/TypeGame.java
@@ -1,14 +1,20 @@
 package game;
 
 import java.util.List;
+import java.util.Scanner;
+import ui.TerminalUI;
 
 public class TypeGame {
 	private final List<String> words;
+	private final Scanner sc;
 	public TypeGame() { 
 		this.words = WordGenerator.gettingSentence();
+		this.sc = new Scanner(System.in);
 	}
 	public void start() {
+		TerminalUI view = new TerminalUI();
 		System.out.println("Starting the game");
 		System.out.println(words);
+		view.displayWords(words,2);
 	}
 }
--- src/ui/TerminalUI.java
+++ src/ui/TerminalUI.java
@@ -3,7 +3,18 @@ package ui;
 import java.util.List;
 
 public class TerminalUI {
-	public static void main(String[] arsg) {
-		// List
+	public void clear() {
+		System.out.println("\033[H\033[2J"); /* ANSI Escape Code */
+		System.out.flush();
+	}
+	public void displayWords(List<String> words, int currentIndex) {
+		for(int i=0;i<words.size();i++) {
+			if(i == currentIndex) {
+				System.out.print("\033[1;32m" + words.get(i) + "\033[0m ");
+			}
+			else {
+				System.out.print(words.get(i));
+			}
+		}
 	}
 }
