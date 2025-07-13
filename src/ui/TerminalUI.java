850d3defcff905efee65bc6bbcb71f4c35c2588a
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2025-07-13 16:03:30 +0530

    Update the Ui #4

--- src/ui/TerminalUI.java
+++ src/ui/TerminalUI.java
@@ -15,7 +15,7 @@ public class TerminalUI {
 				System.out.print("\033[1;32m" + words.get(i) + "\033[0m ");
 			}
 			else {
-				System.out.print(words.get(i));
+				System.out.print(words.get(i) + " ");
 			}
 		}
 	}
@@ -36,9 +36,27 @@ public class TerminalUI {
         	double wpm = (correct / (seconds / 60.0));
         	double acc = (totalChars == 0) ? 100.0 : ((double) correctChars / totalChars) * 100;
 
-        	System.out.printf("     +--------------------+   +---------------------+     \n");
-        	System.out.printf("     |  WPM: %-12.2f|   |  ACC: %-12.2f%%|     \n", wpm, acc);
-        	System.out.printf("     +--------------------+   +---------------------+\n\n     ");
-        	System.out.println("KeyRush CLI\n");
+        	// System.out.printf("     +--------------------+   +---------------------+     \n");
+        	// System.out.printf("     |  WPM: %-12.2f|   |  ACC: %-12.2f%%|     \n", wpm, acc);
+        	// System.out.printf("     +--------------------+   +---------------------+\n\n     ");
+        	printStats(wpm,acc);
+		// System.out.println("KeyRush CLI\n");
     }
+
+    	public static void printStats(double wpm, double acc) {
+    		// ANSI escape codes
+    		String reset = "\033[0m";
+    		String green = "\033[1;32m";
+    		String cyan = "\033[1;36m";
+    		String yellow = "\033[1;33m";
+    		String magenta = "\033[1;35m";
+
+    		// Stats display with color
+    		System.out.println(cyan + "     +--------------------+   +---------------------+");
+    		System.out.printf(green + "     |  WPM: %-12.2f" + cyan + "|   " + yellow + "|  ACC: %-12.2f%%" + cyan + "|%n", wpm, acc);
+    		System.out.println(cyan + "     +--------------------+   +---------------------+" + reset);
+    
+    		System.out.println("\n     " + magenta + "KeyRush CLI" + reset + "\n");
+	}
+
 }
